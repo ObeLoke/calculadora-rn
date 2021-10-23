504a6f9aece38034767d0b9b95e817a1e1c5e8a4
Miguel Veloso <obeloke@hotmail.com>
2021-10-23 01:50:48 -0300

    Diseño Terminado

--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -20,18 +20,26 @@ export const styles = StyleSheet.create({
         fontSize: 30,
         textAlign: 'right',
     },
+    fila: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginBottom: 18,
+        paddingHorizontal: 10,
+    },
     boton: {
         height: 80,
         width: 80,
-        backgroundColor: '#9B9B9B',
+        backgroundColor: '#2D2D2D',
         borderRadius: 100,
         justifyContent: 'center',
+        marginHorizontal: 10
     },
     botonTexto: {
         textAlign: 'center',
-        padding: 10,
+        // padding: 10,
+        paddingRight: 6,
         fontSize: 30,
-        color: 'black',
+        color: 'white',
         fontWeight: '300'
     }
 });
